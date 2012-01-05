184431c95f7fc86bf995f920cc16784b577107bd
Zoran Regvart <zregvart+github@gmail.com>
2012-01-05 17:29:12 +0100

    work so far on AES support, and digital signature rework, take two

---
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/EncryptionImplementation.java
@@ -0,0 +1,60 @@
+package org.apache.pdfbox.pdmodel.encryption;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Key;
+
+import org.apache.pdfbox.exceptions.CryptographyException;
+
+public interface EncryptionImplementation {
+
+    byte[] computeEncryptionKey(final byte[] password, final byte[] o, final int permissions, final byte[] id, final int encRevision, final int length, final boolean encryptMetadata)
+            throws CryptographyException;
+
+    Key computeKey(SecurityHandler securityHandler, long objectNumber, long genNumber) throws CryptographyException;
+
+    byte[] computeOwnerKey(final byte[] ownerPassword, final byte[] userPassword, final int encRevision, final int length) throws CryptographyException;
+
+    byte[] computeUserKey(final byte[] password, final byte[] o, final int permissions, final byte[] id, final int encRevision, final int length, final boolean encryptMetadata)
+            throws CryptographyException;
+
+    InputStream decrypt(Key key, InputStream encrypted) throws CryptographyException, IOException;
+
+    InputStream encrypt(Key key, InputStream clear) throws CryptographyException, IOException;
+
+    byte[] getUserPassword(byte[] bytes, byte[] o, int dicRevision, int dicLength) throws CryptographyException;
+
+    boolean isOwnerPassword(byte[] bytes, byte[] u, byte[] o, int dicPermissions, byte[] documentIDBytes, int dicRevision, int dicLength, boolean encryptMetadata) throws CryptographyException;
+
+    boolean isUserPassword(byte[] bytes, byte[] u, byte[] o, int dicPermissions, byte[] documentIDBytes, int dicRevision, int dicLength, boolean encryptMetadata) throws CryptographyException;
+}
+package org.apache.pdfbox.pdmodel.encryption;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Key;
+
+import org.apache.pdfbox.exceptions.CryptographyException;
+
+public interface EncryptionImplementation {
+
+    byte[] computeEncryptionKey(final byte[] password, final byte[] o, final int permissions, final byte[] id, final int encRevision, final int length, final boolean encryptMetadata)
+            throws CryptographyException;
+
+    Key computeKey(SecurityHandler securityHandler, long objectNumber, long genNumber) throws CryptographyException;
+
+    byte[] computeOwnerKey(final byte[] ownerPassword, final byte[] userPassword, final int encRevision, final int length) throws CryptographyException;
+
+    byte[] computeUserKey(final byte[] password, final byte[] o, final int permissions, final byte[] id, final int encRevision, final int length, final boolean encryptMetadata)
+            throws CryptographyException;
+
+    InputStream decrypt(Key key, InputStream encrypted) throws CryptographyException, IOException;
+
+    InputStream encrypt(Key key, InputStream clear) throws CryptographyException, IOException;
+
+    byte[] getUserPassword(byte[] bytes, byte[] o, int dicRevision, int dicLength) throws CryptographyException;
+
+    boolean isOwnerPassword(byte[] bytes, byte[] u, byte[] o, int dicPermissions, byte[] documentIDBytes, int dicRevision, int dicLength, boolean encryptMetadata) throws CryptographyException;
+
+    boolean isUserPassword(byte[] bytes, byte[] u, byte[] o, int dicPermissions, byte[] documentIDBytes, int dicRevision, int dicLength, boolean encryptMetadata) throws CryptographyException;
+}
---
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/RC4Implementation.java
@@ -0,0 +1,74 @@
+package org.apache.pdfbox.pdmodel.encryption;
+
+import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.pdfbox.exceptions.CryptographyException;
+
+public class RC4Implementation extends BaseEncryptionImplementation implements EncryptionImplementation {
+
+    public Key computeKey(final SecurityHandler securityHandler, final long objectNumber, final long genNumber) throws CryptographyException {
+        final byte[] newKey = generateKeyBase(securityHandler, objectNumber, genNumber);
+
+        // step 3
+        byte[] digestedKey = null;
+        try {
+            final MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(newKey);
+            digestedKey = md.digest();
+        } catch (final NoSuchAlgorithmException e) {
+            throw new CryptographyException(e);
+        }
+
+        // step 4
+        final int length = Math.min(newKey.length, 16);
+        final byte[] finalKey = new byte[length];
+        System.arraycopy(digestedKey, 0, finalKey, 0, length);
+
+        final SecretKey key = new SecretKeySpec(finalKey, "RC4");
+
+        return key;
+    }
+
+}
+package org.apache.pdfbox.pdmodel.encryption;
+
+import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.pdfbox.exceptions.CryptographyException;
+
+public class RC4Implementation extends BaseEncryptionImplementation implements EncryptionImplementation {
+
+    public Key computeKey(final SecurityHandler securityHandler, final long objectNumber, final long genNumber) throws CryptographyException {
+        final byte[] newKey = generateKeyBase(securityHandler, objectNumber, genNumber);
+
+        // step 3
+        byte[] digestedKey = null;
+        try {
+            final MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(newKey);
+            digestedKey = md.digest();
+        } catch (final NoSuchAlgorithmException e) {
+            throw new CryptographyException(e);
+        }
+
+        // step 4
+        final int length = Math.min(newKey.length, 16);
+        final byte[] finalKey = new byte[length];
+        System.arraycopy(digestedKey, 0, finalKey, 0, length);
+
+        final SecretKey key = new SecretKeySpec(finalKey, "RC4");
+
+        return key;
+    }
+
+}

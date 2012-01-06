a3e6ecfa8fe3ae3a4c0ba1ca3b10556f2bce6487
Zoran Regvart <zregvart+github@gmail.com>
2012-01-06 10:35:28 +0100

    fixes merging issues

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/AES256Implementation.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/AES256Implementation.java
@@ -16,21 +16,4 @@ public class AES256Implementation extends BaseAesImplementation implements Encry
     }
 
 }
-package org.apache.pdfbox.pdmodel.encryption;
 
-import java.security.Key;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.pdfbox.exceptions.CryptographyException;
-
-public class AES256Implementation extends BaseAesImplementation implements EncryptionImplementation {
-
-    public Key computeKey(final SecurityHandler securityHandler, final long objectNumber, final long genNumber) throws CryptographyException {
-        final SecretKey key = new SecretKeySpec(securityHandler.encryptionKey, "AES");
-
-        return key;
-    }
-
-}
--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/EncryptionType.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/EncryptionType.java
@@ -5,64 +5,12 @@ import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.cms.CMSAlgorithm;
 
 public enum EncryptionType {
-    RC4_40BIT(40, "RC4", EncryptionType.RC4), RC4_128BIT(128, "RC4", EncryptionType.RC4), RC4_192BIT(192, "RC4", EncryptionType.RC4), RC4_256BIT(256, "RC4", EncryptionType.RC4), DES(56, "DES",
-            new ASN1ObjectIdentifier("1.3.14.3.2.7")), DES3_56BIT(56, "DES", PKCSObjectIdentifiers.des_EDE3_CBC), DES3_112BIT(112, "DES", PKCSObjectIdentifiers.des_EDE3_CBC), DES3_168BIT(168, "DES",
-            PKCSObjectIdentifiers.des_EDE3_CBC), AES_128BIT(128, "AES", CMSAlgorithm.AES128_CBC), AES_192BIT(192, "AES", CMSAlgorithm.AES192_CBC), AES_256BIT(256, "AES", CMSAlgorithm.AES256_CBC);
 
-    public static final ASN1ObjectIdentifier RC4 = PKCSObjectIdentifiers.encryptionAlgorithm.branch("4");
-
-    private final int                        keyLength;
-
-    private final String                     algorithm;
-
-    private final ASN1ObjectIdentifier       algorithmIdentifier;
-
-    private EncryptionType(int keyLength, String algorithm, ASN1ObjectIdentifier algorithmIdentifier) {
-        this.keyLength = keyLength;
-        this.algorithm = algorithm;
-        this.algorithmIdentifier = algorithmIdentifier;
-    }
-
-    public static EncryptionType forLengthAndAlgorithm(int keyLength, String algorithm) {
-        EncryptionType[] values = values();
-
-        for (EncryptionType type : values) {
-            if (type.keyLength == keyLength && type.algorithm.equalsIgnoreCase(algorithm)) {
-                return type;
-            }
-        }
-
-        throw new IllegalArgumentException("Unsupported key length (" + keyLength + ") and/or algorithm: '" + algorithm + "'");
-    }
-
-    public String getAlgorithmName() {
-        return algorithm;
-    }
-
-    public int getKeyLength() {
-        return keyLength;
-    }
-
-    public ASN1ObjectIdentifier getAlgorithmIdentifier() {
-        return algorithmIdentifier;
-    }
-
-    public boolean isAes() {
-        return "AES".equals(algorithm);
-    }
-}
-package org.apache.pdfbox.pdmodel.encryption;
-
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
-import org.bouncycastle.cms.CMSAlgorithm;
-
-public enum EncryptionType {
-    RC4_40BIT(40, "RC4", EncryptionType.RC4), RC4_128BIT(128, "RC4", EncryptionType.RC4), RC4_192BIT(192, "RC4", EncryptionType.RC4), RC4_256BIT(256, "RC4", EncryptionType.RC4), DES(56, "DES",
-            new ASN1ObjectIdentifier("1.3.14.3.2.7")), DES3_56BIT(56, "DES", PKCSObjectIdentifiers.des_EDE3_CBC), DES3_112BIT(112, "DES", PKCSObjectIdentifiers.des_EDE3_CBC), DES3_168BIT(168, "DES",
-            PKCSObjectIdentifiers.des_EDE3_CBC), AES_128BIT(128, "AES", CMSAlgorithm.AES128_CBC), AES_192BIT(192, "AES", CMSAlgorithm.AES192_CBC), AES_256BIT(256, "AES", CMSAlgorithm.AES256_CBC);
-
-    public static final ASN1ObjectIdentifier RC4 = PKCSObjectIdentifiers.encryptionAlgorithm.branch("4");
+    RC4_40BIT(40, "RC4", PKCSObjectIdentifiers.encryptionAlgorithm.branch("4")), RC4_128BIT(128, "RC4", PKCSObjectIdentifiers.encryptionAlgorithm.branch("4")),
+    RC4_192BIT(192, "RC4", PKCSObjectIdentifiers.encryptionAlgorithm.branch("4")), RC4_256BIT(256, "RC4", PKCSObjectIdentifiers.encryptionAlgorithm.branch("4")),
+    DES(56, "DES", new ASN1ObjectIdentifier("1.3.14.3.2.7")), DES3_56BIT(56, "DES", PKCSObjectIdentifiers.des_EDE3_CBC), DES3_112BIT(112, "DES", PKCSObjectIdentifiers.des_EDE3_CBC),
+    DES3_168BIT(168, "DES", PKCSObjectIdentifiers.des_EDE3_CBC), AES_128BIT(128, "AES", CMSAlgorithm.AES128_CBC), AES_192BIT(192, "AES", CMSAlgorithm.AES192_CBC),
+    AES_256BIT(256, "AES", CMSAlgorithm.AES256_CBC);
 
     private final int                        keyLength;
 
@@ -104,3 +52,4 @@ public enum EncryptionType {
         return "AES".equals(algorithm);
     }
 }
+
